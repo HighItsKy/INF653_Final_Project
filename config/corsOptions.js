a44a4e33798e98771d9d0b2e0435fffb0b2f3cb6
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-05-01 20:59:47 -0500

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,8 @@
-const whitelist = ['https://dazzling-snickerdoodle-777101.netlify.app/', 'http://localhost:3500'];
+const allowedOrigins = require('./allowedOrigins');
+
 const corsOptions = {
     origin: (origin, callback)=>{
-        if(whitelist.indexOf(origin) !== -1 || !origin){ //If domain is in whitelist
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){ //If domain is in whitelist
             callback(null, true); 
         } else {
             callback(new Error("Not allowed by CORS"));
