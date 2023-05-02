aad0bfbfc5a079e82ce551e0857b07023db3b60e
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-05-01 20:59:22 -0500

    Create allowedOrigins.js

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'https://dazzling-snickerdoodle-777101.netlify.app/', 
+    'http://localhost:3500',
+    'https://crimson-bramble-thrill.glitch.me'
+];
+
+
+module.exports = allowedOrigins;
