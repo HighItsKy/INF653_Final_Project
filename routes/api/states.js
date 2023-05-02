8324fd2212dd4835cc67a36d7aa41d7a9288c010
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-05-01 20:06:30 -0500

    Added GET routes

--- routes/api/states.js
+++ routes/api/states.js
@@ -2,14 +2,28 @@ const express = require('express');
 const router = express.Router();
 const statesController = require('../../controllers/statesController');
 
-
 router.route('/')
     .get(statesController.getAllStates)
     .post(statesController.createNewState)
     .put(statesController.updateState)
     .delete(statesController.deleteState);
-    
-router.route('/:id')
+
+router.route('/:state')
     .get(statesController.getState);
 
+router.route('/:state/funfact')
+    .get(statesController.getFunfact);
+
+router.route('/:state/capital')
+    .get(statesController.getCapital);
+
+router.route('/:state/nickname')
+    .get(statesController.getNickname);
+
+router.route('/:state/population')
+    .get(statesController.getPopulation);
+
+router.route('/:state/admission')
+    .get(statesController.getAdmission);
+    
 module.exports = router;
